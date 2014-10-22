c87b81e8215fa13c8c319071eda33f637e4eece6
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2014-10-22 10:15:04 -0200

    Removed unused files.

--- apax/Examples/VC/ExCliSrv/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	ExCliSrv.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
